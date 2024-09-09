12035703e1883ed2583b48d90adfe4680dc6feb5
EmilyG27 <133466995+EmilyG27@users.noreply.github.com>
2024-09-08 19:30:10 -0500

    first commit

---
+++ Arithmetic Operations.py
@@ -0,0 +1,12 @@
+#Task 1
+bread = 5
+peanut_butter = 8
+jelly = 6
+
+print(bread + peanut_butter + jelly)
+
+#Task 2
+initial_amount = 10000
+interest = 1.07
+
+print(initial_amount * interest)
---
+++ Showcase your dance moves.py
@@ -0,0 +1,15 @@
+#Task
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2
+mood = input("Are you feeling happy or sad? ")
+
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
